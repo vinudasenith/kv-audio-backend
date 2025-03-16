cd530f9e57efaa065f0d486050bb5dd83c2a87e9
vinudasenith <ha.vinudas@gmail.com>
2025-03-16 07:39:46 +0530

    created whatsapp attribute in user model✅

--- models/user.js
+++ models/user.js
@@ -30,7 +30,11 @@ const userSchema = new mongoose.Schema({
     phone: {
         type: String,
         required: true,
-    }
+    },
+    whatsApp: {
+        type: String,
+        required: true
+    },
 });
 
 const User = mongoose.model("User", userSchema);
